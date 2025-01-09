fd194c47dbc90fce8268d9a689b9821ab70cb934
DoganAliSAN <sandoganali187@gmai.com>
2025-01-09 16:53:59 +0300

    Remove unnecesaary folders

--- visamate_v2/app/imports.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask, request, session, redirect, url_for, render_template, g, jsonify
-
-
-from functions import get_db,verify_password,user_informations,t_types,all_users,get_file_names_without_extensions,t_types_backend,UploadFileForm
